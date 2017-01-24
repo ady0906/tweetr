5b3a42009f0f04c9b2afa990e8c4edf2b16160d7
Adrien Peynichou <adrien.peynichou@gmail.com>
2017-01-24 17:07:42 +0000

    Built and implemented the character counting function

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,4 +1,14 @@
 
 $(document).ready(function() {
   console.log('ready!');
+  $("textarea").on('input', function() {
+    var charCounter = $(this).parent().find('.counter');
+    let tweetLength = 140 - $(this).val().length;
+    charCounter.text(tweetLength.toString());
+    if (tweetLength <= 0) {
+      charCounter.text(tweetLength.toString()).css({'color': 'red'});
+    } else if (tweetLength > 0) {
+      charCounter.text(tweetLength.toString()).css({'color': 'black'});
+    }
+  })
 });
