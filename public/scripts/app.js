59a8a25ed7208aad3cd0421059909985e8485149
Adrien Peynichou <adrien.peynichou@gmail.com>
2017-01-25 18:01:41 +0000

    Implemented the dynamic AJAX calls and fetch functions

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -7,21 +7,19 @@
 
  function createTweetElement(tweetData) {
     let $tweet = $("<article class='newTweet'>");
-
     let $header = $("<header class='header-tweet'>").appendTo($tweet);
-
     let $imgURL = $("<img>").attr("src", tweetData.user.avatars.small).appendTo($header);
     let $author = $("<div class='author'>").text(tweetData.user.name).appendTo($header);
     let $handle = $("<div class='handle'>").text(tweetData.user.handle).appendTo($header);
-
     let $message = $("<div class='message'>").text(tweetData.content.text).appendTo($tweet);
-
     let $time = $("<footer>").text(tweetData.created_at).appendTo($tweet);
+    let $hoverIcons = $("<div class='hoverIcons'>").html("<i class='fa fa-flag' aria-hidden='true'></i>&nbsp;&nbsp;<i class='fa fa-retweet' aria-hidden='true'></i>&nbsp;&nbsp;<i class='fa fa-heart' aria-hidden='true'></i>").appendTo($time);
 
    return $tweet;
   };
 
   function renderTweets(tweets) {
+    $('#tweetsContainer').empty();
     tweets.forEach(function(tweet) {
       createTweetElement(tweet).appendTo($("#tweetsContainer"));
     });
@@ -33,65 +31,48 @@
 
 
  $(document).ready(function() {
-   var data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": {
-        "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-        "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-        "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
-      },
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": {
-        "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
-        "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
-        "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
-      },
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  },
-  {
-    "user": {
-      "name": "Johann von Goethe",
-      "avatars": {
-        "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
-        "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
-        "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
-      },
-      "handle": "@johann49"
-    },
-    "content": {
-      "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
-    },
-    "created_at": 1461113796368
-  }
-];
+
+   function loadTweets() {
+     $.ajax({
+       url: '/tweets',
+       method: 'GET',
+       dataType: 'json',
+       success: function(response) {
+         console.log('Success!');
+       renderTweets(response.reverse());
+       }
+     })
+   };
+
+   loadTweets();
+
+// why is the default behavior not prevented when the function is underneath
+
+    $("form").on("submit", function (event) {
+     event.preventDefault();
+     $.ajax({
+       url: '/tweets',
+       method: 'POST',
+       data: $(this).serialize(),
+       success: loadTweets
+     });
+   });
+
+   $(".newTweet").hover(function() {
+      $(this).addClass("hover");
+    }, function() {
+      $(this).removeClass("hover");
+    }
+  )
+
+});
+
 
   // var $tweet = createTweetElement(tweetData);
 
-   renderTweets(data);
 
-   $(function() {
-     var $button = $('#sendTweet');
-     $button.on('click', function () {
-       
-     })
-   })
 
-});
+
 
  // Test / driver code (temporary). Eventually will get this from the server.
 
