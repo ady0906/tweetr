49185bfb9b367f2eb2b7ac048c364183da394175
Adrien Peynichou <adrien.peynichou@gmail.com>
2017-01-25 22:54:24 +0000

    setting up MongoDB for the application

---
+++ mongo-example.js
@@ -0,0 +1,42 @@
+"use strict";
+
+const MongoClient = require("mongodb").MongoClient;
+const MONGODB_URI = "mongodb://localhost:27017/tweeter";
+
+MongoClient.connect(MONGODB_URI, (err, db) => {
+  if (err) {
+    console.error(`Failed to connect: ${MONGODB_URI}`);
+    throw err;
+  }
+
+  //connection to "tweeter" db starting:
+
+  console.log(`Connect to mongodb: ${MONGODB_URI}`);
+
+  // tweet specific function:
+
+  function getTweets(callback) {
+    db.collection("tweets").find().toArray((err, tweets) => {
+      if (err) {
+        return callback(err);
+      }
+      callback(null, tweets);
+    });
+  }
+
+  // can be invoked later => remember that if `getTweets` passed to another
+  // scope, it still has closure over `db` so it will still work
+
+  getTweets((err, tweets) => {
+    if (err) throw err;
+
+    console.log("Logging each tweet:");
+    for (let tweet of tweets) {
+      console.log(tweet);
+    }
+
+    db.close();
+
+  });
+
+});
